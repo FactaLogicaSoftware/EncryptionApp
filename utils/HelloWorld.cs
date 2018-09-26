8274dd09aa88bbc1bc49a28c74f6f4bd47604c8c
John <johnkellyoxford@gmail.com>
2018-09-26 20:17:00 +0100

    Added bunch of tools
    Mostly command line and need fixing for different drive letters

---
+++ utils/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+
+namespace App
+{
+    class Application
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
