83cbdd8ee675f3d95ba315308c8c584adc370d71
John <johnkellyoxford@gmail.com>
2018-10-05 20:45:00 +0100

    no text

--- src/Backend/AESCryptoManager.cs
+++ src/Backend/AESCryptoManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 
 namespace Encryption_App.Backend
@@ -28,15 +29,23 @@ namespace Encryption_App.Backend
                 aes.Key = key.GetBytes(aes.KeySize / 8);
                 aes.IV = key.GetBytes(aes.BlockSize / 8);
 
-                using (var outFileStream = new FileStream(outFile, FileMode.Create))
+                long len = new FileInfo(inputFile).Length;
+
+                using (var outFileStream = File.Create(outFile))
                 using (var cs = new CryptoStream(outFileStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
-                using (var inFileStream = new FileStream(inputFile, FileMode.Create))
+                using (var inFile = File.OpenRead(inputFile))
                 {
 
-                    sbyte data;
-                    while ((data = (sbyte)inFileStream.ReadByte()) != -1)
-                        cs.WriteByte((byte)data);
+                    using (var br = new BinaryReader(inFile))
+                    {
+
+                        sbyte data;
+                        while ((data = (sbyte)inFile.ReadByte()) != -1)
+                            cs.WriteByte((byte)data);
+
+                    }
                 }
+
             }
         }
 
@@ -45,42 +54,53 @@ namespace Encryption_App.Backend
 
             var saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-            using (var aes = new AesManaged())
+            try
             {
-
-                // AESManaged properties
-                aes.KeySize = 256;
-                aes.BlockSize = 128;
-                aes.Padding = PaddingMode.PKCS7;
-                aes.Mode = CipherMode.CBC;
-
-
-                // Derives a key using PBKDF2 from the password and a salt
-                var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 100000);
-
-
-                // Set actual IV and key
-                aes.Key = key.GetBytes(aes.KeySize / 8);
-                aes.IV = key.GetBytes(aes.BlockSize / 8);
-
-                try
+                using (var aes = new AesManaged())
                 {
-                    using (var outFileStream = new FileStream(outFile, FileMode.Create))
+
+                    // AESManaged properties
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.Mode = CipherMode.CBC;
+
+
+                    // Derives a key using PBKDF2 from the password and a salts
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 100000);
+
+
+                    // Set actual IV and key
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                    long len = new FileInfo(inputFile).Length;
+
+                    using (var outFileStream = File.Create(outFile))
                     using (var cs = new CryptoStream(outFileStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
-                    using (var inFileStream = new FileStream(inputFile, FileMode.Create))
+                    using (var inFile = File.OpenRead(inputFile))
                     {
 
-                        sbyte data;
-                        while ((data = (sbyte)inFileStream.ReadByte()) != -1)
-                            cs.WriteByte((byte)data);
+                        using (var br = new BinaryReader(inFile))
+                        {
+
+                            sbyte data;
+                            while ((data = (sbyte)inFile.ReadByte()) != -1)
+                                cs.WriteByte((byte)data);
+
+                        }
                     }
+
                 }
-                catch (CryptographicException)
-                {
-                    return false;
-                }
-                return true;
             }
+
+            catch (CryptographicException)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
+
