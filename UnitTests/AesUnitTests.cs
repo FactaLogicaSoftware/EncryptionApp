a0ea5fb605e7a37d741d1dbc62be155004ef4825
John <johnkellyoxford@gmail.com>
2018-10-08 17:00:53 +0100

    Update UnitTests/AesUnitTests.cs

--- UnitTests/AesUnitTests.cs
+++ UnitTests/AesUnitTests.cs
@@ -9,7 +9,7 @@ namespace UnitTests
     [TestClass]
     public class AesUnitTests
     {
-        private const string AssetsFolder = @"C:\Users\johnk\source\repos\EncryptionApp\assets\";
+        private readonly string _assetsFolder = Path.GetTempPath() + @"\assets\";
 
         private bool? _testLargeImage;
 
@@ -18,10 +18,16 @@ namespace UnitTests
         {
             _testLargeImage = null;
 
-            using (var fs = new FileStream(AssetsFolder + "testFile.txt", FileMode.Create, FileAccess.Write, FileShare.None))
+            using (var fs = new FileStream(_assetsFolder + "testFile.txt", FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 fs.SetLength(1024 * 1024);
             }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(_assetsFolder, true);
         }
 
         public bool HasPassed => _testLargeImage.GetValueOrDefault();
@@ -31,16 +37,16 @@ namespace UnitTests
         {
             var testEncryptionHandler = new CryptoTools.AesCryptoManager();
             var key = testEncryptionHandler.GenerateKey(256);
-            testEncryptionHandler.EncryptFileBytes(AssetsFolder + "testFile.txt", AssetsFolder + "EncryptedTestFile.txt",
+            testEncryptionHandler.EncryptFileBytes(_assetsFolder + "testFile.txt", _assetsFolder + "EncryptedTestFile.txt",
                 key);
 
-            testEncryptionHandler.DecryptFileBytes(AssetsFolder + "EncryptedTestFile.txt", AssetsFolder + "DecryptedTestFile.txt",
+            testEncryptionHandler.DecryptFileBytes(_assetsFolder + "EncryptedTestFile.txt", _assetsFolder + "DecryptedTestFile.txt",
                 key);
 
             _testLargeImage = false;
             
-            using (var unencryptedFileReader = new BinaryReader(File.OpenRead(AssetsFolder + "LargeImage.png")))
-            using (var decryptedFileReader = new BinaryReader(File.OpenRead(AssetsFolder + "DecryptedLargeImage.png")))
+            using (var unencryptedFileReader = new BinaryReader(File.OpenRead(_assetsFolder + "EncryptedTestFile.txt")))
+            using (var decryptedFileReader = new BinaryReader(File.OpenRead(_assetsFolder + "DecryptedTestFile.txt")))
             {
                 while (true)
                 {
@@ -65,10 +71,10 @@ namespace UnitTests
             var badKey = testEncryptionHandler.GenerateKey(256);
             if (key.SequenceEqual(badKey)) { throw new ExternalException("What the $@#%"); }
 
-            testEncryptionHandler.EncryptFileBytes(AssetsFolder + "LargeImage.png", AssetsFolder + "EncryptedLargeImage.png",
+            testEncryptionHandler.EncryptFileBytes(_assetsFolder + "LargeImage.png", _assetsFolder + "EncryptedLargeImage.png",
                 badKey);
 
-            Debug.Assert(testEncryptionHandler.DecryptFileBytes(AssetsFolder + "EncryptedLargeImage.png", AssetsFolder + "DecryptedLargeImage.png",
+            Debug.Assert(testEncryptionHandler.DecryptFileBytes(_assetsFolder + "EncryptedLargeImage.png", _assetsFolder + "DecryptedLargeImage.png",
                 key) == false, "Used fake key, didn't register it as so");
         }
     }
