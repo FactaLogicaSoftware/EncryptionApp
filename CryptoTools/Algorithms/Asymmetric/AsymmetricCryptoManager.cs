a5943249c5e4a37ae9786c3beff974a0a8284159
nathanmc158 <32514637+nathanmc158@users.noreply.github.com>
2018-10-24 13:50:11 +0100

    Changed it to match with asymmetric logic

--- CryptoTools/Algorithms/Asymmetric/AsymmetricCryptoManager.cs
+++ CryptoTools/Algorithms/Asymmetric/AsymmetricCryptoManager.cs
@@ -11,17 +11,17 @@ namespace FactaLogicaSoftware.CryptoTools.Algorithms.Asymmetric
     {
 
         /// <summary>
-        /// Whether the current aes object is FIPS 140-2 compliant
+        /// Whether the current curves are compliant with pre-defined NIST curves.
         /// </summary>
-        public bool IsFipsCompliant { get; private protected set; }
+        public bool IsNISTCompliant { get; private protected set; }
 
         /// <summary>
         /// If overriden in a derived class, encrypts bytes using the public key
         /// </summary>
         /// <param name="data">The bytes to encrypt</param>
         /// <param name="key">The public key to encrypt with</param>
-        /// <returns></returns>
-        public abstract byte[] EncryptBytesWithPubKey(byte[] data, byte[] key);
+        /// <returns>Encrypted byte Array</returns>
+        public abstract byte[] EncryptBytes(byte[] data, byte[] PublicKey);
 
         /// <summary>
         /// If overriden in a derived class, decrypts bytes using the public key
@@ -29,24 +29,7 @@ namespace FactaLogicaSoftware.CryptoTools.Algorithms.Asymmetric
         /// <param name="data">The bytes to decrypt</param>
         /// <param name="key">The public key to decrypt
         /// the private-key encrypted data</param>
-        /// <returns></returns>
-        public abstract byte[] DecryptBytesWithPubKey(byte[] data, byte[] key);
-
-        /// <summary>
-        /// If overriden in a derived class, encrypts bytes using the public key
-        /// </summary>
-        /// <param name="data">The bytes to encrypt</param>
-        /// <param name="key">The private key to encrypt with</param>
-        /// <returns></returns>
-        public abstract byte[] EncryptBytesWithPrivKey(byte[] data, byte[] key);
-
-        /// <summary>
-        /// If overriden in a derived class, decrypts bytes using the private key
-        /// </summary>
-        /// <param name="data">The bytes to decrypt</param>
-        /// <param name="key">The private key to decrypt
-        /// the public-key encrypted data</param>
-        /// <returns></returns>
-        public abstract byte[] DecryptBytesWithPrivKey(byte[] data, byte[] key);
+        /// <returns>Decrypted byte array</returns>
+        public abstract byte[] DecryptBytes(byte[] data, byte[] PrivateKey);
     }
 }
